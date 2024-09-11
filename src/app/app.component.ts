94e757e3ca7e5aaef8ac39830cadec351b457b5b
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2024-09-11 22:04:58 +0200

    Update AppComponent to include additional Angular Material modules and components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PeriodicTableComponent } from './periodic-table/periodic-table.component';
+import { EditElementDialogComponent } from './edit-element-dialog/edit-element-dialog.component';
+import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button'
+import { MatDialogModule } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PeriodicTableComponent, EditElementDialogComponent, CommonModule, MatTableModule, MatInputModule, MatButtonModule, MatDialogModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
