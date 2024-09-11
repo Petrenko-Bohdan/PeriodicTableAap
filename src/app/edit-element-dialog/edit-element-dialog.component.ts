46948458eee7d7e1b3a3f3093d382a8c6059b21d
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2024-09-11 22:10:02 +0200

    Create EditElementDialogComponent with initial setup

---
+++ src/app/edit-element-dialog/edit-element-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-element-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-element-dialog.component.html',
+  styleUrl: './edit-element-dialog.component.scss'
+})
+export class EditElementDialogComponent {
+
+}
