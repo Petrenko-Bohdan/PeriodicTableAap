9a7e2f81d648f6eff2116ff08503293cefd12169
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2024-09-13 21:52:23 +0200

    Enhanced ElementDataService to manage and provide data using BehaviorSubject and simulate data loading with delay

--- src/app/services/element-data.service.spec.ts
+++ src/app/services/element-data.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ElementDataService } from './element-data.service';
+import { ElementDataService } from '../services/element-data.service';
 
 describe('ElementDataService', () => {
   let service: ElementDataService;
--- src/app/services/element-data.service.ts
+++ src/app/services/element-data.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { PeriodicElement } from '../models/periodic-element.model';
+import { ELEMENT_DATA } from '../../assets/data';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ElementDataService {
+  private elementSubject = new BehaviorSubject<PeriodicElement[]>([]);
+	public elements$ = this.elementSubject.asObservable();
 
-  constructor() { }
+  constructor() {
+    this.loadElements();
+  }
+
+  private loadElements() {
+    setTimeout(() => {
+      this.elementSubject.next(ELEMENT_DATA);
+    }, 2000);
+  }
 }
