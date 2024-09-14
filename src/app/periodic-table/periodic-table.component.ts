9bd5cc8ca9534bb9a1666e041cf335414359b7c8
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2024-09-14 20:01:17 +0200

    - Refactor PeriodicTableComponent to use Observable for dataSource - Add 'actions' column with Edit button to the table - Implement dialog functionality to edit elements - Add necessary imports for MatDialog and MatButtonModule - Load elements in ngOnInit using loadElements method

--- src/app/periodic-table/periodic-table.component.ts
+++ src/app/periodic-table/periodic-table.component.ts
@@ -1,24 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { PeriodicElement } from '../models/periodic-element.model';
 import { ElementDataService } from '../services/element-data.service';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { EditElementDialogComponent } from '../edit-element-dialog/edit-element-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-periodic-table',
   standalone: true,
-  imports: [MatTableModule],
+  imports: [MatTableModule, CommonModule, MatButtonModule],
   templateUrl: './periodic-table.component.html',
   styleUrl: './periodic-table.component.scss',
 })
-export class PeriodicTableComponent {
-  public displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  public dataSource: PeriodicElement[] = [];
+export class PeriodicTableComponent implements OnInit {
+  public displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'actions'];
+  public dataSource: Observable<PeriodicElement[]>;
 
-  constructor(private elementDataService: ElementDataService) {}
+  constructor(private elementDataService: ElementDataService, public dialog:MatDialog) {
+		this.dataSource = this.elementDataService.elements$;
+	}
 
   ngOnInit() {
-    this.elementDataService.elements$.subscribe((data) => {
-      this.dataSource = data;
-    });
+    this.elementDataService.loadElements();
   }
+	
+	editElement(element: PeriodicElement): void {
+		const dialogRef = this.dialog.open(EditElementDialogComponent,{
+			data: element
+		});
+
+		dialogRef.afterClosed().subscribe(result=>{if(result){this.elementDataService.updateElement(result)}});
+	}
 }
